165496750e12ec11213c9be2e26d6858a6f70235
Rory Comiskey <rorycomiskey@hotmail.com>
2018-03-03 15:47:20 +0000

    django filter added to view_products.html

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,8 @@
-from .views import view_products, product_list
+from .views import view_products
 from django.urls import re_path
 
 
 urlpatterns = [
     re_path(r'^category/(?P<hierarchy>.+)/$', view_products, name='view_products'),
-    re_path(r'^list$', product_list),
+    # re_path(r'^list$', product_list),
     ]
--- products/views.py
+++ products/views.py
@@ -4,14 +4,12 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 import django_filters
 from .filters import ProductFilter
 
-# def view_products(request):
-#     products = Product.objects.get_queryset().order_by('id')
-#     return render(request, 'view_products.html', {'products': products })
     
 def view_products(request,hierarchy= None):
     category_slug = hierarchy.split('/')
     parent = None
     root = Category.objects.all()
+    
 
     for slug in category_slug[:-1]:
         parent = root.get(parent=parent, slug = slug)
@@ -19,16 +17,15 @@ def view_products(request,hierarchy= None):
 
     try:
         product = Category.objects.get(parent=parent,slug=category_slug[-1])
+        filter = ProductFilter(request.GET, queryset=Category.get_all_products(product))
     except:
         product = get_object_or_404(Product, slug = category_slug[-1])
-        return render(request, "view_products.html", {'product': product})
+        filter = ProductFilter(request.GET, queryset=Category.get_all_products(product))
+        return render(request, "view_products.html", {'product': product, 'filter': filter })
     else:
-        return render(request, 'view_products.html', {'product': product})
+        return render(request, 'view_products.html', {'product': product, 'filter': filter })
         
-        
-def product_list(request):
-    f = ProductFilter(request.GET, queryset=Product.objects.all())
-    return render(request, 'template.html', {'filter': f})
+
     
 
         
